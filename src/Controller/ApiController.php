90691785985b7fc27f2d138067d0a7f3f89d99a7
AceMisterAll <allanloannchiron@gmail.com>
2022-10-12 08:35:46 +0200

    Api: ajout d'une serie, suppression d'une serie et modifiction complete d'une serie

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\PdoFouDeSerie;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\NotNull;
+
+class ApiController extends AbstractController
+{
+    #[Route('/api/series', name: 'app_api_series', methods: ['GET'])]
+    public function getListeSeries(PdoFouDeSerie $pdoFouDeSerie): Response
+    {
+        $lesSeries = $pdoFouDeSerie->getLesSeries();
+        dump($lesSeries);
+        $TabSeries = [];
+        foreach ($lesSeries as $uneserie) {
+            $TabSeries[] = [
+                'id' => $uneserie['id'],
+                'titre' => $uneserie['titre']
+            ];
+        }
+        return new JsonResponse($TabSeries);
+    }
+
+    #[Route('/api/series/{id}', name: 'app_api_series_id', methods: ['GET'])]
+    public function getUneSerie(PdoFouDeSerie $pdoFouDeSerie, $id): Response
+    {
+        $uneSerie = $pdoFouDeSerie->getUneSerie($id);
+        if ($uneSerie) {
+            dump($uneSerie);
+            $TabSerie = [
+                'id' => $uneSerie['id'],
+                'titre' => $uneSerie['titre'],
+                'resume' => $uneSerie['resume'],
+                'duree' => $uneSerie['duree']
+
+            ];
+            return new JsonResponse($TabSerie);
+        } else {
+            return new JsonResponse(['message' => 'Serie inexistante'], 404);
+        }
+    }
+
+    #[Route('/api/series', name: 'app_api_newSerie', methods: ['POST'])]
+    public function newSerie(Request $request, PdoFouDeSerie $pdoFouDeSerie)
+    {
+        $content = $request->getContent();
+        if (!empty($content)) {
+            $laserie = json_decode($content, true);
+            $laSerieAjouter = $pdoFouDeSerie->setLaSerie($laserie);
+            $tabJson =
+                [
+                    'id' => $laSerieAjouter['id'],
+                    'titre' => $laSerieAjouter['titre'],
+                    'resume' => $laSerieAjouter['resume'],
+                    'duree' => $laSerieAjouter['duree'],
+                    'premiereDiffusion' => $laSerieAjouter['premiereDiffusion'],
+                ];
+        }
+        return new JsonResponse($tabJson, Response::HTTP_CREATED);
+    }
+
+    #[Route('/api/series/{id}', name: 'app_api_series_delete', methods: ['DELETE'])]
+    public function deleteUneSerie(PdoFouDeSerie $pdoFouDeSerie, $id): Response
+    {
+        if (isset($id)) {
+            $pdoFouDeSerie->deleteSerie($id);
+            if ($pdoFouDeSerie) {
+                return new JsonResponse(['message' => 'Serie supprimée'], 200);
+            } else {
+                return new JsonResponse(['message' => 'Serie inexistante'], 404);
+            }
+        } else {
+            return new JsonResponse(['message' => 'Serie non supprime'], 404);
+        }
+    }
+
+    #[Route('/api/series/{id}', name: 'app_api_updateSerie', methods: ['PUT'])]
+    public function updateSerieComplete(Request $request, PdoFouDeSerie $pdoFouDeSerie, $id)
+    {
+        $laserie = $pdoFouDeSerie->getLesSeries($id);
+        if ($laserie == false) {
+            return new JsonResponse(['message' => 'Serie inexistante'], response::HTTP_NOT_FOUND);
+        }
+        $content = $request->getContent();
+        if (!empty($content)) {
+            $laserie = json_decode($content, true);
+            $laSeriemodif = $pdoFouDeSerie->updateSerieComplete($id, $laserie);
+            $tabJson =
+                [
+                    'id' => $laSeriemodif['id'],
+                    'titre' => $laSeriemodif['titre'],
+                    'resume' => $laSeriemodif['resume'],
+                    'duree' => $laSeriemodif['duree'],
+                    'premiereDiffusion' => $laSeriemodif['premiereDiffusion'],
+                ];
+        }
+        return new JsonResponse($tabJson, Response::HTTP_OK);
+    }
+}
--- src/Service/PdoFouDeSeries.php
+++ src/Service/PdoFouDeSeries.php
@@ -7,7 +7,8 @@ class PdoFouDeSerie {
 
     private static $monPdo;
     public function __construct($server, $bdd, $user, $mdp) {
-        PdoFouDeSerie::$monPdo = new PDO($server.';'.$bdd, $user, $mdp);
+        PdoFouDeSerie::$monPdo = new PDO($server.';'.$bdd, $user, $mdp, array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
     }
 
     public function getLesSeries() {
@@ -21,5 +22,54 @@ class PdoFouDeSerie {
         $res->execute();
         $nbSeries = $res->fetch();
         return $nbSeries[0];
+    }
+    public function getUneSerie($id)
+    {
+        $res = PdoFouDeSerie::$monPdo->prepare('SELECT * FROM serie WHERE id = :id');
+        $res->execute(['id' => $id]);
+        $uneSerie = $res->fetch();
+        return $uneSerie;
+    }
+    public function setLaSerie($laserie)
+    {
+        $req = "INSERT INTO serie (titre, resume, duree, premiereDiffusion, image) VALUES (:titre, :resume, :duree, :premiereDiffusion, :image)";
+        $res = PdoFouDeSerie::$monPdo->prepare($req);
+        $res->bindValue(':titre', $laserie['titre'], PDO::PARAM_STR);
+        $res->bindValue(':resume', $laserie['resume'], PDO::PARAM_STR);
+        $res->bindValue(':duree', $laserie['duree'], PDO::PARAM_INT);
+        $res->bindValue(':premiereDiffusion', $laserie['premiereDiffusion'], PDO::PARAM_STR);
+        $res->bindValue(':image', $laserie['image'], PDO::PARAM_STR);
+        $res->execute();
+
+        $req1 = "SELECT * FROM serie WHERE id=LAST_INSERT_ID()";
+        $res1 = PdoFouDeSerie::$monPdo->prepare($req1);
+        $res1->execute();
+        $laLigne = $res1->fetch();
+        return $laLigne;
+    }
+    public function deleteSerie($id)
+    {
+        $req = "DELETE FROM serie WHERE id = :id";
+        $res = PdoFouDeSerie::$monPdo->prepare($req);
+        $res->bindValue(':id', $id, PDO::PARAM_INT);
+        $res->execute();
+    }
+    public function updateSerieComplete($id, $laserie)
+    {
+        $req = "UPDATE serie SET titre = :titre, resume = :resume, duree = :duree, premiereDiffusion = :premiereDiffusion, image = :image WHERE id = :id";
+        $res = PdoFouDeSerie::$monPdo->prepare($req);
+        $res->bindParam(':id', $id, PDO::PARAM_INT);
+        $res->bindParam(':titre', $laserie['titre'], PDO::PARAM_STR);
+        $res->bindParam(':resume', $laserie['resume'], PDO::PARAM_STR);
+        $res->bindParam(':duree', $laserie['duree'], PDO::PARAM_INT);
+        $res->bindParam(':premiereDiffusion', $laserie['premiereDiffusion'], PDO::PARAM_STR);
+        $res->bindParam(':image', $laserie['image'], PDO::PARAM_STR);
+        $res->execute();
+        $req1 = "SELECT * FROM serie WHERE id=:id";
+        $res1 = PdoFouDeSerie::$monPdo->prepare($req1);
+        $res1->bindParam(':id', $id, PDO::PARAM_INT);
+        $res1->execute();
+        $laLigne = $res1->fetch();
+        return $laLigne;
     }
 }
