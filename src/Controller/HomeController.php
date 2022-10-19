f1759dae557bd2c4dcfcea7c5a8f4db959d7769a
Allan Chiron <allanloannchiron@gmail.com>
2022-10-19 09:49:25 +0200

    entité serie, test de l'entite dans le controler home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Serie;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,5 +20,27 @@ class HomeController extends AbstractController
     public function news(): Response
     {
         return $this->render('home/news.html.twig',['nbNews'=>4]);
+    }
+
+    #[Route('/testEntity', name: 'app_testEntity')]
+    public function testEntity(ManagerRegistry $doctrine): Response
+    {
+        // Instanciation d'un objet : création d'une entité
+        $Serie = new Serie();
+        $Serie->setTitre('Naruto');
+        $Serie->setResume("Naruto est un shōnen manga écrit et dessiné par Masashi Kishimoto. Naruto a été prépublié dans l'hebdomadaire Weekly Shōnen Jump de l'éditeur Shūeisha entre septembre 1999 et novembre 2014. La série a été compilée en 72 tomes. La version française du manga est publiée par Kana entre mars 2002 et novembre 2016.");
+        $Serie->setDuree(new \DateTime('12:56:15'));
+        $Serie->setPremiereDiffusion(new \DateTime('1999-09-21'));
+        $Serie->setImage('https://tinyurl.com/2s3r79p9');
+
+        $entityManager = $doctrine->getManager();
+        // On persiste l'entité
+        $entityManager->persist($Serie);
+        // Exécution de la requête
+        $entityManager->flush();
+        return $this->render('home/testEntity.html.twig',
+        [
+            'Serie' => $Serie
+        ]);
     }
 }
